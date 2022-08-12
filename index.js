1dd9273d19590587cad97843115273a5eb29e907
ianvei <iamianveilleux@gmail.com>
2022-08-12 14:05:43 -0400

    correct binary search

--- index.js
+++ index.js
@@ -94,6 +94,96 @@ function contains(object, value) {
     return false;
 }
 
+// function fibs(n) {
+//     let startingArray = [];
+
+//     for(let i = 0; i < n; i++){
+//         if(i == 0){
+//             console.log('hi')
+//             startingArray.push(0)
+//             continue
+//         }
+//         if(i == 1){
+//             console.log('hi')
+//             startingArray.push(1)
+//             continue
+//         }
+//         startingArray.push(startingArray[i-1] + startingArray[i-2]);
+//     }
+//     return startingArray;
+// }
+
+// function fibs(n) {
+//     // const tempArr = [];
+//     if(n == 0) {
+//         return [0];
+//     }
+
+//     if(n == 1) {
+//         return [0, 1];
+//     }
+
+//     const arr = fibs(n-1)
+//     return [...arr, arr[n-1] + arr[n-2]]
+
+  
+
+// }
+
+// console.log(fibs(8));
 // let hasIt = contains(nestedObject, 44); 
 // let doesntHaveIt = contains(nestedObject, "foo"); 
 // console.log(hasIt, doesntHaveIt);
+
+// binary search
+
+const primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
+    41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+// const binarySearch = (array, value) => {
+//     let max = array.length;
+//     let min = 0;
+//     let half = Math.floor(min + max / 2);
+//     console.log(array.length);
+//     console.log(half);
+    
+//     if(array[half] === value) {
+//         return half;
+//     }
+
+//     if(array.length <= 1) {
+//         return array[0];
+//     }
+
+//     if(array[half] < value){
+//         newArray = array.slice(half);
+//     } else {
+//         if(array[half] > value) {
+//             newArray = array.slice(0, half);
+//         }
+//     }
+//     console.log(newArray);
+//     newMin = half;
+//     return binarySearch(newArray, value);
+
+// }
+
+const binarySearch = (array, targetValue, min, max) => {
+    if (max < min) {
+        return -1;
+    }
+
+    let half = Math.floor((min + max) / 2);
+
+    if (array[half] === targetValue) {
+        return half;
+    }
+
+    if(targetValue < array[half]) {
+        return binarySearch(array, targetValue, min, (half-1));
+    } else {
+            return binarySearch(array, targetValue, (half+1), max);
+    }
+}
+
+console.log(binarySearch(primes, 73, 0, primes.length-1))
